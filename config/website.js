44bcf4ef997912cdb8d47d635c423996127a3361
Serra Allgood <serra.allgood@procore.com>
2019-03-25 20:59:15 -0700

    Finish overriding defaults

--- config/website.js
+++ config/website.js
@@ -6,7 +6,7 @@ module.exports = {
   siteTitle: "It's (Serra) Allgood", // Navigation and Site Title
   siteTitleAlt: 'Serra Allgood', // Alternative Site title for SEO
   siteTitleShort: 'Allgood', // short_name for manifest
-  siteHeadline: 'Get to know the (pretty plain) mind of a software psychologist', // Headline for schema.org JSONLD
+  siteHeadline: 'Get to know the (pretty plain) mind of a self-taught software engineer', // Headline for schema.org JSONLD
   siteUrl: 'https://serra.allgood.dev', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   siteLogo: '/logo.png', // Used for SEO and manifest
