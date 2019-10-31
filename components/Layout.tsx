fbed479a22e55c54a73a077069899feea0f800e1
Serra Allgood <serra@allgood.dev>
2019-10-31 16:43:32 -0700

    Add favicons
    Signed-off-by: Serra Allgood <serra@allgood.dev>

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -36,6 +36,10 @@ export const Layout = ({ children }) => {
         <title>It's Allgood, Serra</title>
         <meta name='description' content='Personal professional site for Serra C Allgood' />
         <link href='/static/css/skel.css' rel='stylesheet' />
+        <link rel='apple-touch-icon' sizes='180x180' href='/static/apple-touch-icon.png' />
+        <link rel='icon' type='image/png' sizes='32x32' href='/static/favicon-32x32.png' />
+        <link rel='icon' type='image/png' sizes='16x16' href='/static/favicon-16x16.png' />
+        <link rel='manifest' href='/static/site.webmanifest' />
         <script src='/static/font-awesome/js/all.min.js' />
       </Head>
 
