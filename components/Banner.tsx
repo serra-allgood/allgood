b04ae2c46cf4b2e0167292ef0fc521db8aef7f4e
Serra Allgood <serra@allgood.dev>
2019-11-09 08:00:51 -0800

    Change Banner
    Signed-off-by: Serra Allgood <serra@allgood.dev>

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -2,14 +2,17 @@ export const Banner = () => (
   <section id='banner' className='major'>
     <div className='inner'>
       <header className='major'>
-        <h1>Serra C Allgood</h1>
+        <h1>Serra C. Allgood</h1>
         <h2>
           Self-taught software engineer &amp; non-binary iconoclast
         </h2>
+        <h3>
+          Portfolio self-hosted with Gitea at <a href='https://git.allgood.dev/jump_spider'>git.allgood.dev</a>
+        </h3>
+        <p>
+          any historical pronoun set accepted, <a target='_blank' rel='noopener noreferrer' href='https://allgood.link/pronoun_preferences'>xi/xer/xem/xerself preferred</a>
+        </p>
       </header>
-      <p>
-        any historical pronoun set accepted, <a target='_blank' rel='noopener noreferrer' href='https://allgood.link/pronoun_preferences'>xi/xer/xem preferred</a>
-      </p>
     </div>
   </section>
 )
