2451a6be4c9a7564358a3508297644bc7f6558b5
Serra Allgood <serra@allgood.dev>
2020-03-17 16:14:26 -0700

    Remove some social links, link to git.allgood.dev
    Signed-off-by: Serra Allgood <serra@allgood.dev>

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,18 +1,11 @@
 export const Banner = () => (
-  <section id='banner' className='major'>
-    <div className='inner'>
-      <header className='major'>
+  <section id="banner" className="major">
+    <div className="inner">
+      <header className="major">
         <h1>Serra C. Allgood</h1>
-        <h2>
-          Self-taught software engineer &amp; non-binary iconoclast
-        </h2>
-        <h3>
-          Portfolio self-hosted with Gitea at <a href='https://git.allgood.dev/jump_spider'>git.allgood.dev</a>
-        </h3>
-        <p>
-          any historical pronoun set accepted, she/her preferred
-        </p>
+        <h2>Self-taught software engineer &amp; non-binary iconoclast</h2>
+        <p>any historical pronoun set accepted, she/her preferred</p>
       </header>
     </div>
   </section>
-)
+);
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,58 +1,48 @@
 const Footer = ({ isLoading }) => (
-  <footer id='footer'>
-    <div className='inner'>
-      <ul className={`icons ${isLoading ? 'hidden' : ''}`}>
+  <footer id="footer">
+    <div className="inner">
+      <ul className={`icons ${isLoading ? "hidden" : ""}`}>
         <li>
-          <a href='https://git.allgood.dev/jump_spider' className='icon alt'>
-            <i className='fa fa-gitea' />
-            <span className='label'>Gitea</span>
+          <a href="https://github.com/serra-allgood" className="icon alt">
+            <i className="fa fa-github" />
+            <span className="label">GitHub</span>
           </a>
         </li>
         <li>
-          <a href='https://github.com/serra-allgood' className='icon alt'>
-            <i className='fa fa-github' />
-            <span className='label'>GitHub</span>
+          <a href="https://keybase.io/jump_spider" className="icon alt">
+            <i className="fa fa-keybase" />
+            <span className="label">Keybase</span>
           </a>
         </li>
         <li>
-          <a href='https://keybase.io/jump_spider' className='icon alt'>
-            <i className='fa fa-keybase' />
-            <span className='label'>Keybase</span>
-          </a>
-        </li>
-        <li>
-          <a href='https://with.spider.ink' className='icon alt'>
-            <img src='/static/writefreely.svg' width='20px' />
-            <span className='label'>Blog</span>
-          </a>
-        </li>
-        <li>
-          <a rel='me' href='https://tech.lgbt/@jump_spider' className='icon alt'>
-            <i className='fa fa-mastodon' />
-            <span className='label'>Mastodon</span>
-          </a>
-        </li>
-        <li>
-          <a href='https://twitter.com/serra_allgood' className='icon alt'>
-            <i className='fa fa-twitter' />
-            <span className='label'>Twitter</span>
+          <a href="https://twitter.com/serra_allgood" className="icon alt">
+            <i className="fa fa-twitter" />
+            <span className="label">Twitter</span>
           </a>
         </li>
 
         <li>
-          <a href='https://www.linkedin.com/in/serra-allgood' className='icon alt'>
-            <i className='fa fa-linkedin' />
-            <span className='label'>LinkedIn</span>
+          <a
+            href="https://www.linkedin.com/in/serra-allgood"
+            className="icon alt"
+          >
+            <i className="fa fa-linkedin" />
+            <span className="label">LinkedIn</span>
           </a>
         </li>
       </ul>
-      <ul className='copyright'>
-        <li>&copy;2019 Serra C Allgood</li>
-        <li>Based on a Next.js template by <a href='https://html5up.net'>HTML5 UP</a></li>
-        <li>Powered with Now by <a href='https://zeit.co'>Zeit.co</a></li>
+      <ul className="copyright">
+        <li>&copy; Serra C Allgood</li>
+        <li>
+          Based on a Next.js template by{" "}
+          <a href="https://html5up.net">HTML5 UP</a>
+        </li>
+        <li>
+          Powered with Now by <a href="https://zeit.co">Zeit.co</a>
+        </li>
       </ul>
     </div>
   </footer>
-)
+);
 
-export default Footer
+export default Footer;
