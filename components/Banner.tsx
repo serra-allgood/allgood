0f3b848bacc912314940fc0733eec7d6c46d5845
Serra Allgood <serra@allgood.dev>
2021-05-22 03:46:35 -0700

    Update pronouns
    Signed-off-by: Serra Allgood <serra@allgood.dev>

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,11 +1,11 @@
 export const Banner = () => (
-  <section id="banner" className="major">
-    <div className="inner">
-      <header className="major">
+  <section id='banner' className='major'>
+    <div className='inner'>
+      <header className='major'>
         <h1>Serra C. Allgood</h1>
         <h2>Self-taught software engineer &amp; non-binary iconoclast</h2>
-        <p>any historical pronoun set accepted, she/her preferred</p>
+        <p>she/her</p>
       </header>
     </div>
   </section>
-);
+)
